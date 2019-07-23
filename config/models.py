cad8dcfb5feb05087f337f19bde1274105e138ce
XD-null <289040411@qq.com>
2019-07-23 21:52:28 +0800

    创建Model

---
+++ config/models.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.template.loader import render_to_string
+
+
+class Link(models.Model):
+    STATUS_NORMAL = 1
+    STATUS_DELETE = 0
+    STATUS_ITEMS = (
+        (STATUS_NORMAL, '正常'),
+        (STATUS_DELETE, '删除'),
+    )
+    title = models.CharField(max_length=50, verbose_name="标题")
+    href = models.URLField(verbose_name="链接")  # 默认长度200
+    status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
+    #zip() 函数用于将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的列表。如果各个迭代器的元素个数不一致，则返回列表长度与最短的对象相同，利用 * 号操作符，可以将元组解压为列表。
+    weight = models.PositiveIntegerField(default=1, choices=zip(range(1, 6), range(1, 6)),
+                                         verbose_name="权重",
+                                         help_text="权重高展示顺序靠前")
+
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "友链"
+#侧边栏模型
+class SideBar(models.Model):
+    STATUS_SHOW = 1
+    STATUS_HIDE = 0
+    STATUS_ITEMS = (
+        (STATUS_SHOW, '展示'),
+        (STATUS_HIDE, '隐藏'),
+    )
+    DISPLAY_HTML = 1
+    DISPLAY_LATEST = 2
+    DISPLAY_HOT = 3
+    DISPLAY_COMMENT = 4
+    SIDE_TYPE = (
+        (DISPLAY_HTML, 'HTML'),
+        (DISPLAY_LATEST, '最新文章'),
+        (DISPLAY_HOT, '最热文章'),
+        (DISPLAY_COMMENT, '最近评论'),
+    )
+    title = models.CharField(max_length=50, verbose_name="标题")
+    display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE,
+                                               verbose_name="展示类型")
+    # PositiveIntegerField 正整数
+    content = models.CharField(max_length=500, blank=True, verbose_name="内容",
+                               help_text="如果设置的不是HTML类型，可为空")
+
+    status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
+    owner = models.ForeignKey(User, verbose_name="作者", on_delete=models.DO_NOTHING)
+    created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+
+    class Meta:
+        verbose_name = verbose_name_plural = "侧边栏"
