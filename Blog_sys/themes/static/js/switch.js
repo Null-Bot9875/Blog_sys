131b71c92b24eb834d25a7c7151ef9dab7c20064
XD-null <289040411@qq.com>
2019-08-30 17:20:34 +0800

    响应式图片配置

--- Blog_sys/themes/static/js/switch.js
+++ Blog_sys/themes/static/js/switch.js
@@ -6,17 +6,17 @@
     var switch_editor = function (is_md) {
         if (is_md){
             //console.log('输出markdown界面');
-            $content_md.show();
-            $content_ck.hide();
+            $content_md.show(500);
+            $content_ck.hide(500);
         }
         else {
             //console.log('输出富文本界面');
-            $content_md.hide();
-            $content_ck.show();
+            $content_md.hide(500);
+            $content_ck.show(500);
         }
     }
     $is_md.on('click',function () {
         switch_editor($(this).is(':checked'));
     });
-   // switch_editor($is_md.is(':checked'));
+   switch_editor($is_md.is(':checked'));
 })(jQuery);
---
+++ Blog_sys/themes/static/js/zoom.js
@@ -0,0 +1,4 @@
+console.log($(".main .panel-body p a img"));
+var bg_img = $(".main .panel-body p a img");
+bg_img.removeAttr("style");
+bg_img.addClass("img-responsive");
