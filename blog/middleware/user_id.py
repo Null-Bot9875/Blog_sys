7133451b609e20b8dd07f9ac6b8531b1477f98c4
XD-null <289040411@qq.com>
2019-09-04 16:02:10 +0800

    添加统计量

---
+++ blog/middleware/user_id.py
@@ -0,0 +1,22 @@
+import uuid
+
+USER_KEY = 'uid'
+TEN_YEARS = 60*50*24*365*10
+
+class UserIDMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        uid = self.generate_uid(request)
+        request.uid = uid
+        response = self.get_response(request)
+        #print("uid" +uid+ "request " + str(request)+"response"+str(response))
+        response.set_cookie(USER_KEY,uid,max_age=TEN_YEARS,httponly=True)
+        return response
+    def generate_uid(self,request):
+        try:
+            uid = request.COOKIES[USER_KEY]
+        except KeyError:
+            uid = uuid.uuid4().hex
+        return uid
